59734e9093862ff3347abcdaf7f9babfba9264c3
TheFlameFish <mccoa499@gmail.com>
2023-10-24 18:33:47 -0700

    Created drive subsystem and code for controller driving.

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -6,10 +6,27 @@ package frc.robot;
 
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
+import edu.wpi.first.wpilibj2.command.RunCommand;
+import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
+import frc.robot.subsystems.DriveSubsystem;
+import static frc.robot.constants.*;
+
 
 public class RobotContainer {
+  private DriveSubsystem drive = new DriveSubsystem();
+
+  private final CommandXboxController m_controller = new CommandXboxController(xBoxControllerPort);
+
   public RobotContainer() {
     configureBindings();
+
+    drive.setDefaultCommand(
+      new RunCommand(
+        () -> {
+          drive.arcadeDrive(m_controller.getLeftY(), m_controller.getRightX());
+        },
+        drive)
+    );
   }
 
   private void configureBindings() {}
