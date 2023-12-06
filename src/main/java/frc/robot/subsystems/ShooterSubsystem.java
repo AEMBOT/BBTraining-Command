6a4f82f9bc9f9f6c983d8222a96b0f0549c41e6f
TheFlameFish <mccoa499@gmail.com>
2023-12-05 18:12:08 -0800

    Set up the flywheel in ShooterSubsystem

--- src/main/java/frc/robot/subsystems/ShooterSubsystem.java
+++ src/main/java/frc/robot/subsystems/ShooterSubsystem.java
@@ -5,6 +5,8 @@ import com.revrobotics.ColorSensorV3;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import edu.wpi.first.math.controller.PIDController;
+import edu.wpi.first.math.controller.SimpleMotorFeedforward;
 import edu.wpi.first.wpilibj.I2C;
 import frc.robot.Constants;
 
@@ -12,8 +14,12 @@ public class ShooterSubsystem extends SubsystemBase {
     ColorSensorV3 indexerSensor = new ColorSensorV3(I2C.Port.kMXP);
     
     CANSparkMax conveyorMotor = new CANSparkMax(Constants.conveyorPort, MotorType.kBrushless);
+    CANSparkMax flywheelMotor = new CANSparkMax(Constants.FlywheelMotorPort, MotorType.kBrushless);
 
     Boolean indexerOveride = false;
+
+    PIDController flywheelPID = new PIDController(Constants.shooterKP, Constants.shooterKI, Constants.shooterKP);
+    SimpleMotorFeedforward flywheelFeedForward = new SimpleMotorFeedforward(Constants.shooterKS, Constants.shooterKV,Constants.shooterKA);
 
     @Override 
     public void periodic() {
@@ -26,13 +32,25 @@ public class ShooterSubsystem extends SubsystemBase {
        }
         
     }
-    public void indexerOverideOn(){
+
+    public void IndexerOverideOn(){
         indexerOveride = true;
     }
-    public void indexerOverideOff(){
+
+    public void IndexerOverideOff(){
         indexerOveride = false;
     }
-    public boolean getIndexerOveride(){
+
+    public boolean GetIndexerOveride(){
         return indexerOveride;
+    }
+
+    public void SetFlywheelSpeed(Double speed) {
+        flywheelMotor.setVoltage(flywheelPID.calculate(flywheelMotor.getEncoder().getVelocity(),speed)
+        +flywheelFeedForward.calculate(speed));
+    }
+
+    public void FlywheelOff() {
+        flywheelMotor.setVoltage(0);
     }
 }
